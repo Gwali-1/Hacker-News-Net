9aedd8e3a0ca9cd5b78053a131dc988b7e1bab03
Gwali <gwalisam37@gmail.com>
2023-09-11 13:42:15 +0000

    request to hacker news api returns top stories id and added solution file to current directory

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace FrontPage
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("begin");
+
+            HttpClient client;
+
+            using (client = new HttpClient())
+            {
+                try
+                {
+                    string topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+                    HttpResponseMessage response = await client.GetAsync(topStoriesUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("suncces");
+                        Console.WriteLine("converting");
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        string cleanedREsponse  = jsonResponse.Trim().Substring(1, jsonResponse.Length - 3);
+
+                        Console.WriteLine(cleanedREsponse);
+                        Console.ReadLine();
+                        return;
+                    }
+                    Console.WriteLine($"not success {response.StatusCode}");
+                }
+                catch ( HttpRequestException)
+                {
+                    Console.WriteLine("ERROR: THERE WAS A PROBLEM WITH REQUEST");
+                }
+
+            }
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+ 
+
+        }
+    }
+
+}
