2659790a9d51def26bcdc0213da16e1a9c6acda1
Gwali <gwalisam37@gmail.com>
2023-09-11 15:26:08 +0000

    Hacker news class implemented exposing methods to get top stories as objects

---
+++ HackerNews.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+namespace FrontPage
+{
+    public class HackerNews
+    {
+        private readonly HttpClient? client;
+        private readonly string? topStoriesUrl;
+       
+
+
+        public HackerNews()
+        {
+            client = new HttpClient();
+            topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+        }
+
+        private async Task<List<Story>> getStoryInfo(List<string> ids)
+        {
+            List<Story> stories = new List<Story>();
+
+            foreach (var id in ids)
+            {
+                string url = $"https://hacker-news.firebaseio.com/v0/item/{id.Trim()}.json?print=pretty";
+                HttpResponseMessage story = await new HttpClient().GetAsync(url);
+                string jsonResponse = await story.Content.ReadAsStringAsync();
+               
+                Story storyInfo = JsonConvert.DeserializeObject<Story>(jsonResponse);
+                if(storyInfo == null)
+                {
+                    continue;
+                }
+                stories?.Add(storyInfo);
+            }
+
+            return stories;
+        }
+
+
+        public async Task<List<Story>> TopStoriesJson(int number)
+        {
+            using (client)
+            {
+                try
+                {
+                 
+                    HttpResponseMessage response = await client.GetAsync(this.topStoriesUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("suncces");
+                        Console.WriteLine("converting");
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        string cleanedREsponse = jsonResponse.Trim().Substring(1, jsonResponse.Length - 3);
+                        List<string> storyIDs = cleanedREsponse.Split(",").ToList().GetRange(0, number);
+                        List<Story> TopStories =  await this.getStoryInfo(storyIDs);
+                        return TopStories;
+
+
+
+                    }
+                    Console.WriteLine($"not success {response.StatusCode}");
+                    return new List<Story>{};
+                }
+                catch (HttpRequestException e)
+                {
+                    throw e;
+                }
+
+            }
+
+        }
+
+
+     
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,88 +1,29 @@
-﻿using Newtonsoft.Json;
-namespace FrontPage
+﻿namespace FrontPage
 {
 
-    //public class HackerNews
-    //{
-    //    private readonly HttpClient? client;
-    //    private readonly string? topStoriesUrl;
-    //}
 
-
-    public class Story
-    {
-        public string By { get; set; }
-        public int Score { get; set; }
-        public int   descendants { get; set; }
-        public List<int> kids { get; set; }
-        public string title { get; set; }
-        public string Type { get; set; }
-        public string Url { get; set; }
-        public int time { get; set; }
-        public int id { get; set; }
-    }
 
 
     public class Program
     {
 
-        static async void getStoryInfo(List<string> ids)
-        {
-            List<Story> stories;
-            foreach (var id in ids)
-            {
-                string url = $"https://hacker-news.firebaseio.com/v0/item/{id.Trim()}.json?print=pretty";
-                HttpResponseMessage story = await new HttpClient().GetAsync(url);
-                string jsonResponse = await story.Content.ReadAsStringAsync();
-                Story? storyInfo = JsonConvert.DeserializeObject<Story>(jsonResponse);
-                Console.WriteLine(storyInfo.title);
-                Console.WriteLine(storyInfo.id);
-                Console.WriteLine(storyInfo.title);
-                Console.WriteLine(storyInfo.kids);
-                Console.WriteLine(storyInfo.title);
-
-
-
-            }
-        }
+        
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("begin");
 
-            HttpClient client;
+            HackerNews hn = new HackerNews();
 
-            using (client = new HttpClient())
+            var r = await hn.TopStoriesJson(5);
+            foreach (var item in r)
             {
-                try
-                {
-                    string topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
-                    HttpResponseMessage response = await client.GetAsync(topStoriesUrl);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("suncces");
-                        Console.WriteLine("converting");
-                        string jsonResponse = await response.Content.ReadAsStringAsync();
-                        string cleanedREsponse = jsonResponse.Trim().Substring(1, jsonResponse.Length - 3);
-                        List<string> storyIDs = cleanedREsponse.Split(",").ToList().GetRange(0,10);
-                        getStoryInfo(storyIDs);
-                       
-                 
-                
-                        Console.ReadLine();
-                        return;
-                    }
-                    Console.WriteLine($"not success {response.StatusCode}");
-                }
-                catch ( HttpRequestException)
-                {
-                    Console.WriteLine("ERROR: THERE WAS A PROBLEM WITH REQUEST");
-                }
-
+                Console.WriteLine(item);
             }
 
-            Console.WriteLine("end");
+
             Console.ReadLine();
+           
+     
  
 
         }
