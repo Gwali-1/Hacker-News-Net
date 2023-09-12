379fba2d56d57bc2145d36423cfba0f2cf07f9b6
Gwali <gwalisam37@gmail.com>
2023-09-12 15:33:31 +0000

    folder restructure

---
+++ HNN/Story.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontPage
+{
+    public class Story
+    {
+        public string? By { get; set; }
+        public int Score { get; set; }
+        public int descendants { get; set; }
+        public List<string>? Kids { get; set; }
+        public string? Title { get; set; }
+        public string? Type { get; set; }
+        public string? Url { get; set; }
+        public int Time { get; set; }
+        public long Id { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FrontPage
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HackerNews hn = new HackerNews();
+            var r = await hn.GetComments("8863", 0,2);
+
+            Console.WriteLine(r);
+
+
+            //var t = await HackerNews.SearchItem("8917");        
+            Console.ReadLine();
+        }
+    }
+
+}
